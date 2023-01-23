9dd1b8c3853bf68142746a35b93af7e1a9623f80
Keno <99963700+Kenooooooo@users.noreply.github.com>
2023-01-23 11:31:56 +0100

    Started assignment

---
+++ En nyckel-värde-databas/envlist.ex
@@ -0,0 +1,26 @@
+defmodule EnvList do
+
+  def new() do [] end # Create empty map
+
+  def add(map, key, value) do
+    # CASES
+    # 1. Empty map
+    # 2. Something already in the map, but different key
+    # 3. Same key in map as the one that is added
+  end
+
+  def lookup(map, key) do
+    # CASES
+    # 1. Empty map
+    # 2. Key not in map
+    # 3. Key in map
+  end
+
+  def remove(key, map) do
+    # CASES
+    # 1. Empty map
+    # 2. Key not in map
+    # 3. Key in map
+  end
+
+end
---
+++ En nyckel-värde-databas/envtree.ex
@@ -0,0 +1,11 @@
+defmodule EnvTree do
+
+  def new() do nil end # Create empty map
+  # {:node, key, value, left, right}
+
+  # add
+
+  # lookup
+
+  # remove
+end
