6342af8ddd37744cc6dc025d44bd44c8ec700172
Keno <99963700+Kenooooooo@users.noreply.github.com>
2023-01-31 19:14:02 +0100

    Lookup and add working

--- interpreter/env.ex
+++ interpreter/env.ex
@@ -1,10 +1,12 @@
 defmodule Env do
   def new() do [] end
 
-  def add(id, str, []) do [{id, str}] end
+  def add(id, str, env) do [{id, str} | env] end
 
-  def lookup(id, []) do nil end
-  def lookup(id, env)
+  def lookup(_, []) do nil end
+  def lookup(id, [{id, str} | tail]) do {id, str} end
+  def lookup(id, [head | tail]) do lookup(id, tail) end
 
   def remove(id, []) do [] end
+
 end
