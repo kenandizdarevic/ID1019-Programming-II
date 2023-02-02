6556904eee6dbda51bcdf7d1fae5ac2762c621e8
Keno <99963700+Kenooooooo@users.noreply.github.com>
2023-02-02 08:14:19 +0100

    Fix extension in env.ex

--- interpreter/eager.ex
+++ interpreter/eager.ex
@@ -4,10 +4,11 @@ defmodule Eager do
   def eval(seq) do eval_seq(seq, []) end
 
   def test do
-    seq = [{:match, {:var, :x}, {:atom,:a}},
-          {:match, {:var, :y}, {:cons, {:var, :x}, {:atom, :b}}},
-          {:match, {:cons, :ignore, {:var, :z}}, {:var, :y}},
-          {:var, :z}]
+    seq = [{:match, {:var, :x}, {:atom, :a}},
+            {:case, {:var, :x},
+              [{:clause, {:atom, :b}, [{:atom, :ops}]},
+                {:clause, {:atom, :a}, [{:atom, :yes}]}
+                  ]}]
     eval(seq)
   end
 
@@ -33,6 +34,40 @@ defmodule Eager do
           {:ok, tailStr} ->
             {:ok, {headStr, tailStr}}
         end
+    end
+  end
+
+  def eval_expr({:case, expr, cls}, env) do
+    case eval_expr(expr, env) do
+      :error ->
+        :error
+      {:ok, str} ->
+        eval_cls(cls, str, env)
+    end
+  end
+
+  def eval_expr({:lambda, par, free, seq}, env) do
+    case Env.closure(free, env) do
+      :error ->
+        :error
+      closure ->{:ok, {:closure, par, seq, closure}}
+    end
+  end
+
+  def eval_expr({:apply, expr, args}, env) do
+    case eval_expr(expr, env) do
+      :error ->
+        :error
+      {:ok, {:closure, par, seq, closure}} ->
+        case eval_args(args, env) do
+          :error ->
+            :error
+            {:ok, strs} ->
+              env = Env.args(par, strs, closure)
+              eval_seq(seq, env)
+        end
+        {:ok, _} ->
+          :error
     end
   end
  # ---------------------------------------------------
@@ -89,11 +124,32 @@ defmodule Eager do
   def extract_vars(ptr) do
     extract_vars(ptr, [])
   end
-  def extract_vars({:atm, _}, vars) do vars end
+  def extract_vars({:atom, _}, vars) do vars end
   def extract_vars(:ignore, vars) do vars end
   def extract_vars({:var, var}, vars) do [var | vars] end
   def extract_vars({:cons, head, tail}, vars) do
     extract_vars(tail, extract_vars(head, vars))
+  end
+
+  def eval_cls([], _, _) do :error end
+  def eval_cls([{:clause, ptr, seq} | cls], str, env) do
+    case eval_match(ptr, str, eval_scope(ptr, env)) do
+      :fail ->
+        eval_cls(cls, str, env)
+      {:ok, env} ->
+        eval_seq(seq, env)
+    end
+end
+
+  def eval_args(args, env) do eval_args(args, env, []) end
+  def eval_args([], _, strs) do  {:ok, Enum.reverse(strs)} end
+  def eval_args([head | tail], env, strs) do
+    case eval_expr(head, env) do
+      :error ->
+        :error
+      {:ok, str} ->
+        eval_args(tail, env, [str | strs])
+    end
   end
 
 end
