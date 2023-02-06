9a7087fd8252e56d520200daf3542f1dc0ba0398
Keno <kenandi@kth.se>
2023-02-06 20:56:20 +0100

    Done

--- higher/high.ex
+++ higher/high.ex
@@ -1,8 +1,12 @@
 defmodule High do
 
   def test do
-    listInt = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    double_five_animal(listInt, :double)
+    listInt = [1, 2, 3, 4, 6, 7, 8]
+    listDog = [:cow, :dog, :cat]
+
+    f = fn(x) -> x > 5 end
+    filter(listInt, f)
+
   end
 
   def double([]) do [] end
@@ -21,14 +25,59 @@ defmodule High do
   end
 
   def double_five_animal([], _) do [] end
-  def double_five_animal(list, func) do
-    case func do
+  def double_five_animal([head | tail], f) do
+    case f do
       :double ->
-        double(list)
+        [head * 2 | double_five_animal(tail, f)]
       :five ->
-        five(list)
+        [head + 5 | double_five_animal(tail, f)]
       :animal ->
-        animal(list)
+        if head == :dog do
+          [:fido | double_five_animal(tail, f)]
+        else
+          [head | double_five_animal(tail, f)]
+        end
     end
   end
+
+  def apply_to_all([], _) do [] end
+  def apply_to_all([head | tail], f) do
+    [f.(head) | apply_to_all(tail, f)]
+  end
+
+  def sum([]) do 0 end
+  def sum([head | tail]) do head + sum(tail) end
+
+  def prod([]) do 1 end
+  def prod([head | tail]) do head * prod(tail) end
+
+  def fold_right([], base, _) do base end
+  def fold_right([head | tail], base, f) do
+    f.(head, fold_right(tail, base, f))
+  end
+
+  def fold_left([], acc, _) do acc end
+  def fold_left([head | tail], acc, f) do
+    fold_left(tail, f.(head, acc), f)
+  end
+
+  def odd([]) do [] end
+  def odd([head | tail]) do
+    if rem(head, 2) == 1 do
+      [head | odd(tail)]
+    else
+      odd(tail)
+    end
+  end
+
+  def filter([], _) do [] end
+  def filter([head | tail], f) do
+    case f.(head) do
+      true ->
+        [head | filter(tail, f)]
+      false ->
+         filter(tail, f)
+      end
+  end
+
 end
