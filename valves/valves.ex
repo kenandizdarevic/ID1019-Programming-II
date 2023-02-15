e5bdd55aec4f22b8a2406a1ed09e1e1fcd21bc1e
Keno <kenandi@kth.se>
2023-02-15 20:26:31 +0100

    Fix search and insert

--- valves/valves.ex
+++ valves/valves.ex
@@ -1,13 +1,19 @@
 defmodule Valves do
 
-  def task(t) do
+  def task() do
     start = :AA
-    rows = File.stream!("input.csv")
-    #rows = sample()
-    parse(rows)
+    #rows = File.stream!("input.csv")
+    rows = sample()
+    map = parse(rows)
+    time = 30
+    closed = []
+    opened = []
+
+    {max_rate, max_path} = search(start, time, closed, opened, 0, map, [])
+
+    IO.puts("Maximum rate: #{max_rate}")
+    IO.inspect(max_path)
   end
-
-
 
   ## turning rows
   ##
@@ -42,4 +48,55 @@ defmodule Valves do
      "Valve II has flow rate=0; tunnels lead to valves AA, JJ",
      "Valve JJ has flow rate=21; tunnel leads to valve II"]
   end
+
+  # ---------- ARGUMENTS ----------
+  # valve: current valve being considered
+  # time: time left
+  # closed: valves which are currently closed
+  # opened: valves which are currently open
+  # rate: current rate of flow
+  # map:
+  # path: current path being taken
+
+
+  # No time left
+  def search(_, 0, _, _, _, _, path) do {0, path} end
+  # All valves are opened
+  def search(_, time, [], _, rate, _, path) do
+    {rate * time, path}
+  end
+
+  def search(valve, time, closed, opened, rate, map, path) do
+    {valveRate, tunnels} = Map.get(map, valve)
+
+    {maxRate, maxPath} =
+      case Enum.member?(closed, valve) do
+        true ->
+          [removed | remaining] = closed
+          added = insert(opened, valve)
+          {subMaxRate, subMaxPath} =
+            search(removed, time - 1, remaining, added, rate + valveRate, map, [valve | path])
+          {subMaxRate + rate, subMaxPath}
+        false ->
+          {rate * time, path}
+      end
+
+    {finalRate, finalPath} =
+      Enum.reduce(tunnels, {maxRate, maxPath}, fn(nextValve, {maxRate, maxPath}) ->
+        {subRate, subPath} =
+          search(nextValve, time - 1, closed, opened,rate, map, path)
+        subRate = subRate + rate
+        if subRate > maxRate do
+          {subRate, subPath}
+        else
+          {maxRate, maxPath}
+        end
+      end)
+
+    {finalRate, finalPath}
+  end
+
+  def insert(list, element) do [element | list] end
+
+
 end
