ae34d78ca97bcd720edd972e7d77223b4672270e
Keno <kenandi@kth.se>
2023-02-23 18:59:41 +0100

    Added strength

--- philosophers/dinner.ex
+++ philosophers/dinner.ex
@@ -1,7 +1,8 @@
 defmodule Dinner do
-  def start(), do: spawn(fn -> init() end)
+  def start(hunger), do: spawn(fn -> init(hunger) end)
 
-  def init() do
+  def init(hunger) do
+
     c1 = Chopstick.start()
     c2 = Chopstick.start()
     c3 = Chopstick.start()
@@ -10,26 +11,28 @@ defmodule Dinner do
 
     ctrl = self()
 
-    Philosopher.start(5, c1, c2, "Arendt", ctrl)
-    Philosopher.start(5, c2, c3, "Hypatia", ctrl)
-    Philosopher.start(5, c3, c4, "Simone", ctrl)
-    Philosopher.start(5, c4, c5, "Elisabeth", ctrl)
-    Philosopher.start(5, c5, c1, "Ayn", ctrl)
+    Philosopher.start(hunger, 5, c1, c2, "Arendt", ctrl)
+    Philosopher.start(hunger, 5, c2, c3, "Hypatia", ctrl)
+    Philosopher.start(hunger, 5, c3, c4, "Simone", ctrl)
+    Philosopher.start(hunger, 5, c4, c5, "Elisabeth", ctrl)
+    Philosopher.start(hunger, 5, c5, c1, "Ayn", ctrl)
 
-    wait(5, [c1, c2, c3, c4, c5])
+    t0 = :os.system_time(:second)
+    wait(5, [c1, c2, c3, c4, c5], t0)
   end
 
   # Everyone is done, terminate alla chopsticks
   # Philosophers are done, have returned :ok to motherprocess
-  def wait(0, chopsticks) do
+  def wait(0, chopsticks, t0) do
     Enum.each(chopsticks, fn(c) -> Chopstick.terminate(c) end)
+    IO.puts("Dinner completed in #{:os.system_time(:second) - t0}s")
   end
 
   # Motherprocess can send :abort to kill everything
-  def wait(n, chopsticks) do
+  def wait(n, chopsticks, t0) do
     receive do
       :done ->
-        wait(n - 1, chopsticks)
+        wait(n - 1, chopsticks, t0)
       :abort ->
         Process.exit(self(), :kill)
     end
--- philosophers/philosopher.ex
+++ philosophers/philosopher.ex
@@ -5,54 +5,58 @@ defmodule Philosopher do
     :timer.sleep(:rand.uniform(time))
   end
 
-  def start(hunger, left, right, name, ctrl) do
-    spawn_link(fn() -> dreaming(hunger, left, right, name, ctrl) end)
+  def start(hunger, strength, left, right, name, ctrl) do
+    spawn_link(fn() -> dreaming(hunger, strength, left, right, name, ctrl) end)
   end
 
-  def dreaming(0, _, _, name, ctrl) do
-    IO.puts("#{name} is satisfied!")
+  def dreaming(0, strength, _left, _right, name, ctrl) do
+    IO.puts("#{name} is satisfied, strength is #{strength}!")
     send(ctrl, :done)
   end
 
-  def dreaming(hunger, left, right, name, ctrl) do
+  def dreaming(_hunger, 0, _left, _rigjt, name, ctrl) do
+      IO.puts("#{name} has no strength left!")
+      send(ctrl, :done)
+  end
+
+  def dreaming(hunger, strength, left, right, name, ctrl) do
     IO.puts("#{name} is dreaming!")
     sleep(500)
     IO.puts("#{name} is awake!")
-    waiting(hunger, left, right, name, ctrl)
+    waiting(hunger, strength, left, right, name, ctrl)
   end
 
-  def eating(hunger, left, right, name, ctrl, ref) do
+  def eating(hunger, strength, left, right, name, ctrl, ref) do
     sleep(100)
     Chopstick.return(left, ref)
     Chopstick.return(right, ref)
     IO.puts("#{name} has eaten one portion!")
-    dreaming(hunger - 1, left, right, name, ctrl)
+    dreaming(hunger - 1, strength, left, right, name, ctrl)
   end
 
   # Chopstick.request() will only return :ok
-  def waiting(hunger, left, right, name, ctrl) do
+  def waiting(hunger, strength, left, right, name, ctrl) do
     IO.puts("#{name} is waiting for chopsticks!")
 
     ref = make_ref()
     Chopstick.async(left, ref)
     Chopstick.async(right, ref)
-
     case Chopstick.sync(ref, 1000) do
       :ok ->
         IO.puts("#{name} recieved one chopstick!")
-        sleep(1000)
+        sleep(3000)
         case Chopstick.sync(ref, 1000) do
           :ok ->
             IO.puts("#{name} is now able to eat!")
-            eating(hunger, left, right, name, ctrl, ref)
+            eating(hunger, strength, left, right, name, ctrl, ref)
           :no ->
-            IO.puts("#{name} has aborted wait for chopstick!")
+            IO.puts("#{name} has aborted wait for chopstick, strength is #{strength}!")
             Chopstick.return(left, ref)
-            dreaming(hunger, left, right, name, ctrl)
+            dreaming(hunger, strength - 1, left, right, name, ctrl)
         end
       :no ->
-        IO.puts("#{name} has aborted wait for chopstick!")
-        dreaming(hunger, left, right, name, ctrl)
+        IO.puts("#{name} has aborted wait for chopstick, strength is #{strength}!")
+        dreaming(hunger, strength - 1, left, right, name, ctrl)
     end
   end
 end
