f3ed5d3b59782eca1609018fd88c8869090570b3
Kenan Dizdarevic <kenandi@kth.se>
2023-02-27 17:38:50 +0100

    Done

--- shunting/moves.ex
+++ shunting/moves.ex
@@ -1,7 +1,38 @@
 defmodule Moves do
 
-  def single(move, state) do end
+  # Move is a binary tuple
+  # First element is :one or :two
+  # Second element is an integer
+  # {:one, 2} or {:two, 1}
 
-  def sequence(moves, state) do end
+  # Decide by pattern-matching which track is involved
+  # For a track decide wheter wagons are moved on or from (n + or -)
+  # Use main/2 when moving wagons from main track
+
+  def single({_, 0}, {main, one, two}) do {main, one, two} end
+  # Move n wagons to track one
+  def single({:one, n}, {main, one, two}) when n > 0 do
+    {0, remain, take} = Train.main(main, n)
+    {remain, Train.append(take, one), two}
+    # {take, remain} = Train.split(main, length(main) - n)
+    # {take, Train.append(remain, one), two}
+  end
+  def single({:one, n}, {main, one, two}) when n < 0 do
+    n = abs(n)
+    {Train.append(main, Train.take(one, n)), Train.drop(one, n), two}
+  end
+  def single({:two, n}, {main, one, two}) when n > 0 do
+    {0, remain, take} = Train.main(main, n)
+    {remain, one, Train.append(take, two)}
+  end
+  def single({:two, n}, {main, one, two}) when n < 0 do
+    n = abs(n)
+    {Train.append(main, Train.take(two, n)), one, Train.drop(two, n)}
+  end
+
+  def sequence([], state) do [state] end
+  def sequence([head | tail], state) do
+    [state | sequence(tail, single(head, state))]
+  end
 
 end
